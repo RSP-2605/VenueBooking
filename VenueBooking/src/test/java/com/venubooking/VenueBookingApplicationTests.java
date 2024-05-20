1a186afa8e7179737bebc656b621e058baaa97e2
rahulsingh.parihar <rahulsingh.parihar@ACL-232.amla.io>
2024-05-20 16:50:21 +0530

    SpringBoot VenueBooking

---
+++ VenueBooking/src/test/java/com/venubooking/VenueBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venubooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenueBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
